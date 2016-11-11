542c2d57337a4b8d93c1b09e25929427735cd2eb
pod-proteinom <alta0815@yandex.ru>
2016-11-11 18:01:14 +0300

    Implemented second task

--- index.js
+++ index.js
@@ -12,6 +12,33 @@ app.get('/task2A', function(req, res) {
     res.end(result.toString());
 });
 
+app.get('/task2B', (req, res) => {
+    const fullname = req.query.fullname;
+    const decodeFullname = decodeURIComponent(fullname);
+    if (!decodeFullname || /[0-9]+/g.test(decodeFullname)) {
+        res.end('Invalid fullname');
+    }
+
+    const words = decodeFullname.replace(/\s\s+/g, ' ').split(' ');
+
+    if (words.length > 3) {
+        res.end('Invalid fullname');
+    } else {
+        if (words.length == 1) {
+            res.end(words[0]);
+        } else if (words.length == 2) {
+            const firstName = words[0].slice(0, 1);
+            const familyName = words[1];
+            res.end(`${familyName} ${firstName}.`);
+        } else {
+            const firstName = words[0].slice(0, 1);
+            const middleName = words[1].slice(0, 1);
+            const familyName = words[2];
+            res.end(`${familyName} ${firstName}. ${middleName}.`);
+        }
+    }
+});
+
 app.listen(3000, function() {
     console.log('App listening on port 3000!');
 });
